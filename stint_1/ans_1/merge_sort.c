9a76b12ecda8b8b257fbe165eb934ac00311870b
Anmol Agarwal <anmolagarwal4453@gmail.com>
2020-10-19 12:22:22 +0530

    Weird stuff happening

--- stint_1/ans_1/merge_sort.c
+++ stint_1/ans_1/merge_sort.c
@@ -14,6 +14,16 @@
 #include <math.h>
 #define part printf("--------------------------------\n");
 typedef long long LL;
+//Regular bold text
+#define BBLK "\e[1;30m"
+#define BRED "\e[1;31m"
+#define BGRN "\e[1;32m"
+#define BYEL "\e[1;33m"
+#define BBLU "\e[1;34m"
+#define BMAG "\e[1;35m"
+#define BCYN "\e[1;36m"
+#define BWHT "\e[1;37m"
+#define ANSI_RESET "\x1b[0m"
 
 /////////////////////////////////////////////////////
 int *org_arr, *thread_arr;
@@ -84,10 +94,9 @@ void merge_halves(int arr[], LL l, LL m, LL r)
         right_arr[j] = arr[m + 1 + j];
     }
 
-    /* Merge the temp arrays back into arr[l..r]*/
-    i = 0; // Initial index of first subarray
-    j = 0; // Initial index of second subarray
-    k = l; // Initial index of merged subarray
+    i = 0;
+    j = 0;
+    k = l;
     while (i < sz1 && j < sz2)
     {
         if (left_arr[i] <= right_arr[j])
@@ -200,37 +209,41 @@ void *threaded_merge_sort(void *args)
             // }
             return NULL;
         }
+        else
+        {
 
-        LL m = l + (r - l) / 2;
-        pthread_t tid1, tid2;
+            LL m = l + (r - l) / 2;
+            pthread_t tid1, tid2;
 
-        struct thread_data a1;
-        a1.l = l;
-        a1.r = m;
-        a1.ptr_to_arr = arr;
-        pthread_create(&tid1, NULL, threaded_merge_sort, &a1);
+            struct thread_data a1;
+            a1.l = l;
+            a1.r = m;
+            a1.ptr_to_arr = arr;
+            pthread_create(&tid1, NULL, threaded_merge_sort, &a1);
 
-        //sort right half array
-        struct thread_data a2;
-        a2.l = m + 1;
-        a2.r = r;
-        a2.ptr_to_arr = arr;
-        pthread_create(&tid2, NULL, threaded_merge_sort, &a2);
+            //sort right half array
+            struct thread_data a2;
+            a2.l = m + 1;
+            a2.r = r;
+            a2.ptr_to_arr = arr;
+            pthread_create(&tid2, NULL, threaded_merge_sort, &a2);
 
-        //wait for the two halves to get sorted
-        pthread_join(tid1, NULL);
-        pthread_join(tid2, NULL);
+            //wait for the two halves to get sorted
+            pthread_join(tid1, NULL);
+            pthread_join(tid2, NULL);
 
-        merge_halves(arr, l, m, r);
+            merge_halves(arr, l, m, r);
+            return NULL;
+        }
+        printf(BRED "Weird stuff here Miami\n" ANSI_RESET);
     }
-    return NULL;
 }
 
 void process_merge_sort(int *shared_arr, int l, int r)
 {
     if (l < r)
     {
-        //printf("l is %d and r is %d\n", l, r);
+        //As long as array size is not one
         LL sz = r - l + 1;
 
         if (sz < 5)
@@ -260,42 +273,58 @@ void process_merge_sort(int *shared_arr, int l, int r)
             // }
             return;
         }
-
-        LL m = l + (r - l) / 2;
-
-        int pid1, pid2;
-
-        pid1 = fork();
-        if (pid1 == 0)
-        {
-            process_merge_sort(shared_arr, l, m);
-            _exit(1);
-        }
         else
         {
 
-            pid2 = fork();
-            if (pid2 == 0)
+            LL m = l + (r - l) / 2;
+
+            int pid1, pid2;
+
+            pid1 = fork();
+            if (pid1 == 0)
             {
-                process_merge_sort(shared_arr, m + 1, r);
+                process_merge_sort(shared_arr, l, m);
+
+                //Why there is a reason to use _exit() instead of exit() for this question ?
+                //https://stackoverflow.com/a/3657687/6427607
                 _exit(1);
+            }
+            else if (pid1 == -1)
+            {
+                perror("Fork_1: ");
+                exit(-1);
             }
             else
             {
-                int status;
-                waitpid(pid1, &status, 0);
-                waitpid(pid2, &status, 0);
-                merge_halves(shared_arr, l, m, r);
+
+                pid2 = fork();
+                if (pid2 == 0)
+                {
+                    process_merge_sort(shared_arr, m + 1, r);
+                    _exit(1);
+                }
+                else if (pid2 == -1)
+                {
+                    perror("Fork_2: ");
+                    exit(-1);
+                }
+                else
+                {
+                    int status;
+                    waitpid(pid1, &status, 0);
+                    waitpid(pid2, &status, 0);
+                    merge_halves(shared_arr, l, m, r);
+                }
             }
         }
     }
 }
 void print_arr(int *a, int n)
 {
-
+    printf("\n");
     for (int i = 0; i < n; i++)
     {
-        printf(" elem is %d\n ", a[i]);
+        printf("%d ", a[i]);
     }
     printf("\n");
 }
@@ -311,22 +340,30 @@ void copy_arr(int *arr, int *send_arr, LL n)
 int main()
 {
     LL n;
+    printf("Enter value of n\n");
     scanf("%lld", &n);
-    //printf("n is %d\n", n);
     /////////////////////////////////////////////////////////////
+    //for process merge sort
     int *shared_arr = get_shared_mem(n + 1);
+
+    //original array
     org_arr = (int *)malloc(sizeof(int) * n);
+
+    //making copy for threaded merge sort
     thread_arr = (int *)malloc(sizeof(int) * n);
 
     //////////////////////////////////////////////////////////////
 
     int arr_size = n;
+    printf("Input n elements\n");
     for (int i = 0; i < n; i++)
     {
         scanf("%d", &org_arr[i]);
     }
+    ///////////////////////////////////////////////////////////////
     copy_arr(org_arr, shared_arr, n);
     copy_arr(org_arr, thread_arr, n);
+    //////////////////////////////////////////////////////////////////
     long double st, en;
     ////////////////////////////////////////////////////////////////////
     //Concurrent MERGE SORT
@@ -345,8 +382,8 @@ int main()
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
     en = ts.tv_nsec / (1e9) + ts.tv_sec;
-    printf("time taken = %Lf\n", en - st);
     long double t1 = en - st;
+    printf("time taken = %Lf seconds\n", t1);
     part;
     ///////////////////////////////////////////////////////////////////////
     ////////////////////////////////////////////////////////////////////
@@ -369,11 +406,12 @@ int main()
     print_arr(thread_arr, n);
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
     en = ts.tv_nsec / (1e9) + ts.tv_sec;
-    printf("time taken = %Lf\n", en - st);
     long double t2 = en - st;
+    printf("time taken = %Lf\n", t2);
     part;
     ///////////////////////////////////////////////////////////////////////
-     ////////////////////////////////////////////////////////////////////
+
+    ////////////////////////////////////////////////////////////////////
     //Normal MERGE SORT
     part;
     printf("Before NORMAL merge sort, contents are\n");
@@ -382,15 +420,28 @@ int main()
 
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
     st = ts.tv_nsec / (1e9) + ts.tv_sec;
-    merge_sort(org_arr,0,n-1);
+    merge_sort(org_arr, 0, n - 1);
     printf("After NORMAL merge sort, contents are\n");
     print_arr(org_arr, n);
     clock_gettime(CLOCK_MONOTONIC_RAW, &ts);
     en = ts.tv_nsec / (1e9) + ts.tv_sec;
-    printf("time taken = %Lf\n", en - st);
     long double t3 = en - st;
+    printf("time taken = %Lf\n", t3);
     part;
     ///////////////////////////////////////////////////////////////////////
+
+    //t1->process
+    //t2->threaded
+    //t3->normal
+
+    printf("-------------Summary--------------\n");
+    printf("Time for process merge sort is %Lf\n", t1);
+    printf("Time for threaded merge sort is %Lf\n", t2);
+    printf("Time for normal merge sort is %Lf\n", t3);
+
+    printf("Normal mergesort take %Lf of the time taken by Process merge_sort\n", t3 / t1);
+    printf("Normal mergesort take %Lf of the time taken by Threaded merge_sort\n", t3 / t2);
+    printf("Threaded mergesort take %Lf of the time taken by Process merge_sort\n", t2 / t1);
 
     return 0;
 }
